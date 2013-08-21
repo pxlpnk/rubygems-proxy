06aa3e589b64bcab6a68e436220d819d0b3c806b
Andreas Tiefenthaler <at@an-ti.eu>
2013-08-21 10:47:18 +0200

    api endpoint for /quick/ and a catchall

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require 'bundler'
 Bundler.setup(:defaults)
 Bundler.require
@@ -6,10 +8,7 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 require 'fileutils'
-
 require 'uri'
-require 'net/https'
-require 'pry'
 
 require './lib/fetcher'
 
@@ -18,25 +17,30 @@ RUBYGEMS_PORT = 443
 CACHE_PATH = "#{Dir.pwd}/cache"
 VERSION = '0.0.1'
 
-FileUtils.mkdir_p(CACHE_PATH + '/gems')
-
-
 get '/' do
   "RubyGems proxy cache v#{VERSION}"
 end
 
-get '/api/v1/*'do
-  redirect_uri = [RUBYGEMS_URL.to_s, request.fullpath].join
-  logger.info "Redirecting to: #{redirect_uri}"
-  redirect redirect_uri, 303
-end
-
-get '/gems/*' do
-
-  fetch_url = URI.join(RUBYGEMS_URL, request.fullpath)
-  destination_file = [CACHE_PATH, request.fullpath].join
+get '/quick/Marshal.4.8/*' do
+    fetch_url = URI.join(RUBYGEMS_URL, request.fullpath)
+  destination_file = File.join(CACHE_PATH, request.fullpath)
   gem = Fetcher.new(fetch_url, destination_file)
   gem.fetch
 
   send_file destination_file
+end
+
+get '/gems/*' do
+  fetch_url = URI.join(RUBYGEMS_URL, request.fullpath)
+  destination_file = File.join(CACHE_PATH, request.fullpath)
+  gem = Fetcher.new(fetch_url, destination_file)
+  gem.fetch
+
+  send_file destination_file
+end
+
+get '/*'do
+  redirect_uri = [RUBYGEMS_URL.to_s, request.fullpath].join
+  logger.info "Redirecting to: #{redirect_uri}"
+  redirect redirect_uri, 303
 end
